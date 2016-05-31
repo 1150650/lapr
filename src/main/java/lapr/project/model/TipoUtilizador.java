f4d427e9c7beac3d9040c0223345ce1ce69abd97
Diogo <Diogo@asus>
2016-05-31 10:42:32 +0100

    Adicionar e alterar classe com Enum dos Tipos de Utilizador [implementation]

---
+++ src/main/java/lapr/project/model/TipoUtilizador.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+public enum TipoUtilizador {
+    ORGANIZADOR {
+        public String toString() {
+            return "Organizador";
+        }
+    },
+    FAE {
+        public String toString() {
+            return "FAE";
+        }
+    },
+    GESTOR_DE_EXPOSICOES {
+        public String toString() {
+            return "Gestor de Exposições";
+        }
+    },
+    REPRESENTANTE_DE_EXPOSITOR {
+        public String toString() {
+            return "Representante de expositor";
+        }
+    };
+
+}
