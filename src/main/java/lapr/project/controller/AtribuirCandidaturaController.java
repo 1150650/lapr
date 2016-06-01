141280b8133c68c636d589079504636aac9c3b7a
Diogo <1150650@isep.ipp.pt>
2016-06-01 10:17:06 +0100

    Testar identificação do autor [teste]

--- src/main/java/lapr/project/controller/AtribuirCandidaturaController.java
+++ src/main/java/lapr/project/controller/AtribuirCandidaturaController.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lapr.project.controller;
 
 import java.util.List;
 import javax.swing.JOptionPane;
 import lapr.project.model.*;
 
-/**
- *
- * @author SimãoPedro
- */
+
 public class AtribuirCandidaturaController {
 
     private Exposicao exposicaoSelecionada;
