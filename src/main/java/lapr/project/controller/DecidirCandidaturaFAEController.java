5ee581da347e97972a4d7fa51236a6599aaba66f
Simão Pedro <1150928@isep.ipp.pt>
2016-06-07 11:44:48 +0100

    Implementação dos mecanismos para atribuir candidaturas [UC-4][Implementation]

--- src/main/java/lapr/project/controller/DecidirCandidaturaFAEController.java
+++ src/main/java/lapr/project/controller/DecidirCandidaturaFAEController.java
@@ -5,38 +5,53 @@ import java.util.ArrayList;
 import java.util.List;
 import lapr.project.model.Candidatura;
 import lapr.project.model.CentroExposicoes;
+import lapr.project.model.Exposicao;
+import lapr.project.model.FAE;
 import lapr.project.model.ListaAtribuicoes;
-
+import lapr.project.model.ListaAvaliacoes;
+import lapr.project.model.ListaCandidaturas;
+import lapr.project.model.ListaFAE;
+import lapr.project.model.Utilizador;
 
 public class DecidirCandidaturaFAEController {
 
-    private Avaliacao decisao;
-    private CentroExposicoes ce;
-    private Candidatura c;
-    private ListaAtribuicoes listaAtribuicoes;
+    private FAE fae;
 
-    public DecidirCandidaturaFAEController(CentroExposicoes centroexpo) {
-        this.ce = centroexpo;
+    private Candidatura candidatura;
+
+    private Avaliacao avaliacao;
+
+    private ListaCandidaturas lstCand;
+
+    private ListaAvaliacoes lstAval;
+
+    public DecidirCandidaturaFAEController(String id, Exposicao expo) {
+        this.fae = expo.getListaFAE().obterFAEId(id);
+        this.lstCand = fae.getListacandidatura();
     }
 
-
-    public void selecionarCandidatura(Candidatura candidatura) {
-        this.c = candidatura;
+    public void selecionarCandidatura(Candidatura a) {
+        this.candidatura = a;
     }
 
-    public void guardarDecisao(String decisao, String justificacao) {
-        this.decisao.setAvaliacao(decisao);
-        this.decisao.setJustificacao(justificacao);
+    public String getInfo() {
+        return candidatura.apresentarDados();
     }
 
-    public void novaDecisao(Avaliacao decisao) {
-        this.decisao = decisao;
-
+    public void getListaAvaliacoes() {
+        this.lstAval = candidatura.getListaAvaliacoes();
     }
 
-    public void guardarAvaliacao() {
-        this.c.getListaAvaliacoes().addAvaliacao(decisao);
+    public void novaAvaliacao(boolean avaliacao, String justificacao) {
+        this.avaliacao = new Avaliacao(avaliacao, justificacao);
     }
-    
-    
+
+    public void setFAEAvaliacao(int[] conhecimentoFAE) {
+        avaliacao.setConhecimentosFAE(conhecimentoFAE);
+    }
+
+    public void adicionarAvaliacao(Avaliacao a) {
+        lstAval.addAvaliacao(avaliacao);
+    }
+
 }
