cf4433de1d6ae414b7a006f834200a9864c875a0
Filipe <1150524@isep.ipp.pt>
2016-06-09 12:32:58 +0100

    Codificação de Definir FAE(Sem estados) [UC2][Implementação]

---
+++ src/main/java/lapr/project/controller/DefinirFAEController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+import java.util.List;
+import lapr.project.model.*;
+
+/**
+ *
+ * @author Filipe <FilipeCorreia.1150524>
+ */
+public class DefinirFAEController {
+
+    private RegistoExposicoes registoExposicoes;
+    private CentroExposicoes centroExposicoes;
+    private RegistoUtilizadores registoUtilizadores;
+    private List<Exposicao> listaExposicoes;
+    private List <Utilizador> listaUtilizadores;
+    private Exposicao exposicao;
+    private ListaFAE listaFAE;
+    private ListaOrganizadores listaOrganizadores;
+    private FAE fae;
+
+    public DefinirFAEController(CentroExposicoes centroExposicoes){
+        this.centroExposicoes = centroExposicoes;
+    }
+    
+    public List getExposicoesOrganizador(Utilizador utilizador){
+        listaExposicoes = registoExposicoes.getExposicoesOrganizador(utilizador);
+        return listaExposicoes;
+    }
+    
+    public List getListaUtilizadores(){
+        listaUtilizadores = registoUtilizadores.getRegistoUtilizadores();
+        return listaUtilizadores;
+    }
+    
+    public void addFAE(Utilizador utilizador, Exposicao exposicao, String id){
+        this.exposicao = exposicao;
+        listaFAE = this.exposicao.getListaFAE();
+        listaOrganizadores = this.exposicao.getListaOrganizadores();
+        if(!this.exposicao.getListaOrganizadores().isUserOrganizador(utilizador)){
+            fae = listaFAE.addFAE(utilizador, id);
+        }
+    }
+    
+    public void registaFAE(){
+        listaFAE.registaFAE(fae);
+    }
+    
+}
