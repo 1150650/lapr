16a122b5349641a5147f6dc031451e221f4c42b9
JOAO <1150479@isep.ipp.pt>
2016-06-15 10:01:36 +0100

    Classe RegistarExposicaoController [UC-O1]  - [Implementação]

---
+++ src/main/java/lapr/project/controller/RegistarExposicaoController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+import java.util.Date;
+import lapr.project.model.CentroExposicoes;
+import lapr.project.model.Exposicao;
+import lapr.project.model.Organizador;
+import lapr.project.model.RegistoExposicoes;
+import lapr.project.model.RegistoUtilizadores;
+import lapr.project.model.Utilizador;
+
+/**
+ *
+ * @author JOAO
+ */
+public class RegistarExposicaoController {
+    private RegistoExposicoes registoExposicoes;
+     private CentroExposicoes centroExposicoes;
+
+    private Exposicao exposicao;
+    
+    public RegistarExposicaoController(CentroExposicoes centroexpo ){
+        this.centroExposicoes=centroexpo ;
+    }
+    
+    public void novaExposicao (){
+        this.exposicao=centroExposicoes.getListaExposicoes().novaExposicao();
+    }
+    
+    public void setDados (String titulo ,String textoDescritivo,Date dataInicio,Date dataFim,String local){
+        exposicao.setLocal(local);
+        exposicao.setPeriodo(dataInicio, dataFim);
+        exposicao.setTextoDescritivo(textoDescritivo);
+        exposicao.setTitulo(titulo);
+    }
+    
+    public RegistoUtilizadores getListaUtilizadores (){
+       return this.centroExposicoes.getRegistoUtilizadoresConf();
+    }
+    
+    public void adicionarOrganizador (Utilizador selecionado){
+        Organizador u =new Organizador ();
+        u.setUtilizador(selecionado);
+        exposicao.getListaOrganizadores().adicionarOrganizador(u);
+    }
+    
+    public void validaRegistaExposicao (){
+        centroExposicoes.getListaExposicoes().adicionarExposicao(exposicao);
+    }
+    
+    public void setEstadoExposicaoCriada (){
+        exposicao.setExposicaoCriada();
+    }
+    
+    
+}
