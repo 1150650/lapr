d9bb4dc4bcefadb00d32181f8f9196b42f5d95ef
JOAO <1150479@isep.ipp.pt>
2016-06-15 12:31:27 +0100

    Implementação CriarDemonstraçãoUI [UC-08] - [Implementação]

---
+++ src/main/java/lapr/project/ui/CriarDemonstraçãoUI.java
@@ -0,0 +1,220 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.ui;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import lapr.project.controller.CriarDemonstraçãoController;
+import lapr.project.model.CentroExposicoes;
+import lapr.project.model.Recurso;
+
+/**
+ *
+ * @author JOAO
+ */
+public class CriarDemonstraçãoUI extends JDialog {
+
+    private JTextField txtDescricao;
+    private JButton btnAdicionarRecurso;
+    private ModeloListaRecursos lstRecursos;
+    private CriarDemonstraçãoController contr;
+    private CentroExposicoes centroexpo;
+    private JanelaPrincipal framePai;
+
+    public CriarDemonstraçãoUI(JanelaPrincipal framePai, CentroExposicoes ce) {
+
+        super(framePai, "Registar Exposicao", true);
+        this.centroexpo = ce;
+        this.contr = new CriarDemonstraçãoController(ce);
+        this.framePai = framePai;
+
+        criarComponentes();
+
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        pack();
+        setResizable(false);
+        setLocationRelativeTo(framePai);
+        setVisible(true);
+    }
+
+    private void criarComponentes() {
+
+        JPanel p1 = criarPainelNorte();
+        JPanel p2 = criarPainelCentro();
+        JPanel p3 = criarPainelSul();
+
+        add(p1, BorderLayout.NORTH);
+        add(p2, BorderLayout.CENTER);
+        add(p3, BorderLayout.SOUTH);
+    }
+
+    private JPanel criarPainelNorte() {
+
+        JPanel p2 = criarPainelDescricao();
+        JPanel pNorte = new JPanel(new BorderLayout());
+        pNorte.add(p2, BorderLayout.CENTER);
+        return pNorte;
+    }
+
+    private JPanel criarPainelCentro() {
+        JPanel p1 = criarPainelRecurso();
+
+        JPanel pCentro = new JPanel(new BorderLayout());
+        pCentro.add(p1, BorderLayout.CENTER);
+
+        return pCentro;
+    }
+
+    private JPanel criarPainelSul() {
+        JButton btnOK = criarBotaoOK();
+        getRootPane().setDefaultButton(btnOK);
+
+        JButton btnCancelar = criarBotaoCancelar();
+
+        JPanel p = new JPanel();
+
+        p.add(btnOK);
+        p.add(btnCancelar);
+
+        return p;
+    }
+
+    private JPanel criarPainelDescricao() {
+        JLabel lbl = new JLabel("Descricao:", JLabel.RIGHT);
+
+        final int CAMPO_LARGURA = 10;
+        txtDescricao = new JTextField(CAMPO_LARGURA);
+
+        JPanel p = new JPanel();
+
+        p.add(lbl);
+        p.add(txtDescricao);
+
+        return p;
+    }
+
+    private JPanel criarPainelRecurso() {
+        final int NUMERO_LINHAS = 1, NUMERO_COLUNAS = 2;
+        final int INTERVALO_HORIZONTAL = 20, INTERVALO_VERTICAL = 0;
+        JPanel p = new JPanel(new GridLayout(NUMERO_LINHAS,
+                NUMERO_COLUNAS,
+                INTERVALO_HORIZONTAL,
+                INTERVALO_VERTICAL));
+        lstRecursos = new ModeloListaRecursos(contr.getListaRecursos());
+        JList lstCompleta = new JList(lstRecursos);
+
+        btnAdicionarRecurso = criarBotaoAdiconarRecurso(lstCompleta);
+
+        p.add(criarPainelLista("Lista de Recursos:",
+                lstCompleta,
+                lstRecursos,
+                btnAdicionarRecurso));
+
+        return p;
+    }
+
+    private JPanel criarPainelLista(
+            String tituloLista,
+            JList lstLista,
+            ModeloListaRecursos modeloLista,
+            JButton btnSuperior) {
+        JLabel lblTitulo = new JLabel(tituloLista, JLabel.LEFT);
+        lstLista.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane scrPane = new JScrollPane(lstLista);
+
+        JPanel p = new JPanel(new BorderLayout());
+
+        final int MARGEM_SUPERIOR = 20, MARGEM_INFERIOR = 20;
+        final int MARGEM_ESQUERDA = 20, MARGEM_DIREITA = 20;
+        p.setBorder(BorderFactory.createEmptyBorder(MARGEM_SUPERIOR,
+                MARGEM_ESQUERDA,
+                MARGEM_INFERIOR,
+                MARGEM_DIREITA));
+
+        p.add(lblTitulo, BorderLayout.NORTH);
+        p.add(scrPane, BorderLayout.CENTER);
+
+        JPanel pBotoes = criarPainelBotoes(btnSuperior);
+        p.add(pBotoes, BorderLayout.SOUTH);
+        return p;
+    }
+
+    private JPanel criarPainelBotoes(JButton btnSuperior) {
+        final int NUMERO_LINHAS = 2, NUMERO_COLUNAS = 1;
+        final int INTERVALO_HORIZONTAL = 0, INTERVALO_VERTICAL = 10;
+        JPanel p = new JPanel(new GridLayout(NUMERO_LINHAS,
+                NUMERO_COLUNAS,
+                INTERVALO_HORIZONTAL,
+                INTERVALO_VERTICAL));
+
+        final int MARGEM_SUPERIOR = 10, MARGEM_INFERIOR = 0;
+        final int MARGEM_ESQUERDA = 0, MARGEM_DIREITA = 0;
+        p.setBorder(BorderFactory.createEmptyBorder(MARGEM_SUPERIOR,
+                MARGEM_ESQUERDA,
+                MARGEM_INFERIOR,
+                MARGEM_DIREITA));
+
+        p.add(btnSuperior);
+
+        return p;
+    }
+
+    private JButton criarBotaoOK() {
+        JButton btn = new JButton("Registar Demonstração");
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                dispose();
+
+            }
+        });
+
+        return btn;
+    }
+
+    private JButton criarBotaoCancelar() {
+        JButton btn = new JButton("Cancelar");
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        return btn;
+    }
+
+    private JButton criarBotaoAdiconarRecurso(JList lstCompleta) {
+        JButton btn = new JButton("Adicionar Recursos");
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    ModeloListaRecursos m = (ModeloListaRecursos) lstCompleta.getModel();
+                    Recurso r = (Recurso) lstCompleta.getSelectedValue();
+                    contr.adicionarRecurso(r);
+
+                } catch (NumberFormatException ex) {
+
+                }
+            }
+        });
+
+        return btn;
+    }
+
+}
